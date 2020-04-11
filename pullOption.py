006f5500f110afc4e7535aa593bb94ff7053e34c
npiacen <47162082+npiacen@users.noreply.github.com>
2020-04-11 10:43:07 -0500

    Create pullOption.py

---
+++ pullOption.py
@@ -0,0 +1,17 @@
+# pullOption.py
+# Date Created: 4/11/20
+# Date Last Modified: XX
+# By: Nick Piacente
+
+# get an option quote using yahoo_fin
+
+from yahoo_fin import options
+from yahoo_fin import stock_info
+
+stock = 'AAPL'
+
+aaplCallChain = options.get_calls(stock)
+currentPrice=stock_info.get_live_price(stock)
+
+aaplOptions = options.get_calls(stock).plot(x='Strike',y=['Bid','Ask'], xlim=[.5*currentPrice,1.5*currentPrice],title='Bid/Ask Spread for {}'.format(stock))
+aaplOptions.axvline(currentPrice, color='green', ls='--')
