bc0e50393397ec536d67fa3aaf5918fe5e1314d9
npiacen <47162082+npiacen@users.noreply.github.com>
2020-04-14 16:45:24 -0500

    Update pullOption.py

--- pullOption.py
+++ pullOption.py
@@ -8,10 +8,11 @@
 from yahoo_fin import options
 from yahoo_fin import stock_info
 
-stock = 'PLUG'
+stock = 'AAPL'
 
 aaplCallChain = options.get_calls(stock)
 currentPrice=stock_info.get_live_price(stock)
+info = stock_info.get_quote_table(stock)
 
 aaplOptions = options.get_calls(stock).plot(x='Strike',y=['Bid','Ask'], xlim=[.5*currentPrice,1.5*currentPrice],title='Bid/Ask Call Spread for {}'.format(stock))
 aaplOptions.axvline(currentPrice, color='green', ls='--')
