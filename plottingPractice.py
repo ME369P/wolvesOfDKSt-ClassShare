d6c0b4f8c3a896dcad9949c08a4f8b1e009b6b62
dcayll <60239356+dcayll@users.noreply.github.com>
2020-04-26 16:07:31 -0500

    Update plottingPractice.py

--- plottingPractice.py
+++ plottingPractice.py
@@ -56,158 +56,168 @@ import getYahooData as yd
 #     return ax
 
 
-class plotOptions(tkinter.Tk):
+# class plotOptions(tkinter.Tk):
     
-    Risk_num = 0
-    Budget_num = 0
-    startFlag = False
-    
-    def __init__(self, df, *args, **kwargs):
-        tkinter.Tk.__init__(self, *args, **kwargs)
-        self.title("Put Option Strategy")
-        self.geometry('1500x800+100+100')
-        # self._setup_inputGUI()
-        # self._setup_gui()
-        # self._questions = questions[:]
-        # self._answers = answers
-        # self._show_next_question()
-    
-    # def store_data():
-    #     print("Risk: %s\nBudget: %s" % (Risk.get(), Budget.get()))
-    #     Risk_num = float(Risk.get())
-    #     Budget_num = float(Budget.get())
-    #     return Risk_num, Budget_num
-    
-    def _setup_inputGUI(self):
-        # risk level input
-        self.tkinter.Label(textFrame, text="Risk Level").grid(row=1, column=1)#side=tkinter.LEFT, anchor=tkinter.SW)
-        Risk = tkinter.Entry(textFrame)
-        Risk.grid(row=1, column=2)#side=tkinter.LEFT, anchor=tkinter.SW)
-        # budget level input
-        self.Label(textFrame, text="Budget").grid(row=2, column=1)#side=tkinter.LEFT, anchor=tkinter.SW)
-        Budget = tkinter.Entry(textFrame)
-        Budget.grid(row=2, column=2)#side=tkinter.LEFT, anchor=tkinter.SW)
-        self.Risk_num = Risk.get()
-        self.Budget_num = Budget.get()
-        go = tkinter.Button(textFrame, text='Enter')#, command=store_data)
-        go.grid(row=3, column=1, columnspan=2)
-    
-    
-    def startGUI(root):
-        # initialize TK GUI window
+Risk_num = 0
+Budget_num = 0
+startFlag = False
 
-        
-        # input_root = tkinter.Tk()
-        # input_root = tkinter.Toplevel()
-        # input_root.wm_title("Inputs")
-        textFrame = tkinter.Frame(root, relief = tkinter.RAISED, borderwidth=5)
-        textFrame.pack()
-        return textFrame
-    
-    
-    
-    ###########################################
-    ############# Get Inputs ##################
-    ###########################################
-    
-    def store_data(Risk_num, Budget_num):
-        print("Risk: %s\nBudget: %s" % (Risk_num, Budget_num))
-        startFlag = True
-        return True
-    
-    def GetInputs(textFrame):
-        tkinter.Label(textFrame, text="Risk Level").grid(row=1, column=1)#side=tkinter.LEFT, anchor=tkinter.SW)
-        Risk = tkinter.Entry(textFrame)
-        Risk.grid(row=1, column=2)#side=tkinter.LEFT, anchor=tkinter.SW)
-        tkinter.Label(textFrame, text="Budget").grid(row=2, column=1)#side=tkinter.LEFT, anchor=tkinter.SW)
-        Budget = tkinter.Entry(textFrame)
-        Budget.grid(row=2, column=2)#side=tkinter.LEFT, anchor=tkinter.SW)
-        Risk_num = Risk.get()
-        Budget_num = Budget.get()
-        go = tkinter.Button(textFrame, text='Enter', command=plotOptions.store_data(Risk_num, Budget_num))
-        go.grid(row=3, column=1, columnspan=2)
-        
-        return Risk_num, Budget_num
-    
+# def __init__(self, df, *args, **kwargs):
+#     tkinter.Tk.__init__(self, *args, **kwargs)
+#     self.title("Put Option Strategy")
+#     self.geometry('1500x800+100+100')
+#     # self._setup_inputGUI()
+#     # self._setup_gui()
+#     # self._questions = questions[:]
+#     # self._answers = answers
+#     # self._show_next_question()
+
+# def store_data():
+#     print("Risk: %s\nBudget: %s" % (Risk.get(), Budget.get()))
+#     Risk_num = float(Risk.get())
+#     Budget_num = float(Budget.get())
+#     return Risk_num, Budget_num
+
+def startGUI():
+    _root = tkinter.Tk()
+    _root.title("Put Option Strategy")
+    _root.geometry('1500x800+100+100')
+    return _root
+
+
+
+def _setup_inputGUI(self):
+    # risk level input
+    self.tkinter.Label(textFrame, text="Risk Level").grid(row=1, column=1)#side=tkinter.LEFT, anchor=tkinter.SW)
+    Risk = tkinter.Entry(textFrame)
+    Risk.grid(row=1, column=2)#side=tkinter.LEFT, anchor=tkinter.SW)
+    # budget level input
+    self.Label(textFrame, text="Budget").grid(row=2, column=1)#side=tkinter.LEFT, anchor=tkinter.SW)
+    Budget = tkinter.Entry(textFrame)
+    Budget.grid(row=2, column=2)#side=tkinter.LEFT, anchor=tkinter.SW)
+    self.Risk_num = Risk.get()
+    self.Budget_num = Budget.get()
+    go = tkinter.Button(textFrame, text='Enter')#, command=store_data)
+    go.grid(row=3, column=1, columnspan=2)
+
+
+def startTextInput(_root):
+    # initialize TK GUI window
 
     
+    # input_root = tkinter.Tk()
+    # input_root = tkinter.Toplevel()
+    # input_root.wm_title("Inputs")
+    _textFrame = tkinter.Frame(_root, relief = tkinter.RAISED, borderwidth=5)
+    _textFrame.pack()
+
+    return _textFrame
+
+
+
+###########################################
+############# Get Inputs ##################
+###########################################
+
+def store_data(Risk_num, Budget_num, textFrame):
+    print("Risk: {}\nBudget: {}".format(Risk_num, Budget_num))
+    tkinter.Label(textFrame, text="Risk: {}\nBudget: {}".format(Risk_num, Budget_num)).grid(row=4, column=1, columnspan=2)#side=tkinter.LEFT, anchor=tkinter.SW)
+    startFlag = True
+    return True
+
+def GetInputs(_textFrame):
+    tkinter.Label(_textFrame, text="Risk Level").grid(row=1, column=1)#side=tkinter.LEFT, anchor=tkinter.SW)
+    Risk = tkinter.Entry(_textFrame)
+    Risk.grid(row=1, column=2)#side=tkinter.LEFT, anchor=tkinter.SW)
+    tkinter.Label(_textFrame, text="Budget").grid(row=2, column=1)#side=tkinter.LEFT, anchor=tkinter.SW)
+    Budget = tkinter.Entry(_textFrame)
+    Budget.grid(row=2, column=2)#side=tkinter.LEFT, anchor=tkinter.SW)
+    Risk_num = Risk.get()
+    Budget_num = Budget.get()
+    go = tkinter.Button(_textFrame, text='Enter', command=store_data(Risk_num, Budget_num, _textFrame))
+    go.grid(row=3, column=1, columnspan=2)
     
-    # T = tkinter.Text(root, height=2, width=30)
-    # T.pack()
-    # T.insert(tkinter.END, Risk_num)
+    return Risk_num, Budget_num
+
+
+
+
+# T = tkinter.Text(root, height=2, width=30)
+# T.pack()
+# T.insert(tkinter.END, Risk_num)
+
+
+
+############################################
+######### Import data and Plot #############
+############################################
+def printDetailFig(self, BestTicker):
+    # initialize figure and axes objects using pyplot for detail plot
+    detail_fig = plt.figure(figsize= (6,4), dpi = 100)
+    detail_ax = detail_fig.add_subplot(111)
+    yd.getDetailedQuote('DOW', detail_ax)
+    # put detail axes into tkinter GUI 
+    canvas = FigureCanvasTkAgg(detail_fig, master=root)
+    canvas.get_tk_widget().pack(side=tkinter.LEFT, anchor=tkinter.NW)#, fill=tkinter.X)#, expand=1)
     
-    
-    
-    ############################################
-    ######### Import data and Plot #############
-    ############################################
-    def printDetailFig(self, BestTicker):
-        # initialize figure and axes objects using pyplot for detail plot
-        detail_fig = plt.figure(figsize= (6,4), dpi = 100)
-        detail_ax = detail_fig.add_subplot(111)
-        yd.getDetailedQuote('DOW', detail_ax)
-        # put detail axes into tkinter GUI 
-        canvas = FigureCanvasTkAgg(detail_fig, master=root)
-        canvas.get_tk_widget().pack(side=tkinter.LEFT, anchor=tkinter.NW)#, fill=tkinter.X)#, expand=1)
-        
-    
-    
-    def printParetoCurve(self):
-        # initalize figure and axes objects using pyplot for pareto curve
-        pareto_fig = plt.figure(figsize=(8,7), dpi=100)
-        pareto_ax = pareto_fig.add_subplot(111)
-        # stockPareto, bestPick = yd.getOptionsData(0.9, 10000)
-        stockPareto = pd.read_pickle('stockParetaData0425.pk1')
-        stockPareto.plot(kind='scatter',x='POP',y='Potential Gain Multiple Contracts', legend = 'Stock Name', ax = pareto_ax)
-        pareto_ax.set_title('Pareto Curve for Best Options (Puts)')
-        pareto_ax.set_xlabel('Probability of Profit (%)')
-        pareto_ax.set_ylabel('Premium Collected')
-        
-        
-        # put pareto curve axes into tkinter GUI
-        canvas = FigureCanvasTkAgg(pareto_fig, master=root)
-        canvas.get_tk_widget().pack(side=tkinter.RIGHT, anchor=tkinter.NE)#, fill=tkinter.Y)#, expand=1)
-    
-    
-    # canvas.create_oval(10, 10, 80, 80, outline="#f11",fill="#1f1", width=2)
-    
-    
-    
-    
-    
-    
-    
-    
-    # # plot data to respective axes objects
-    # # pareto_ax.plot(t, s)
-    
-    
-    # ##
-    
-    
-    
-    
-    
-    # # adds in toolbar for paretoCurve plot
-    # toolbar = NavigationToolbar2Tk(canvas, root)
-    # toolbar.update()
-    # canvas._tkcanvas.pack(side=tkinter.LEFT)#, fill=tkinter.BOTH, expand=1)
-    
-    
-    def callback():
-        print("click!")
-    
-    
-    
-    def refresh():
-        print("refresh")
-    
-    # b = tkinter.Button(master=root, text="Quit", command=callback)
-    # b.pack(side=tkinter.LEFT, anchor=tkinter.SW)
-    # refresh = tkinter.Button(master=root, text = "Refresh", command=refresh)
-    # refresh.pack(side=tkinter.LEFT, anchor=tkinter.SW)
+
+
+def printParetoCurve(self):
+    # initalize figure and axes objects using pyplot for pareto curve
+    pareto_fig = plt.figure(figsize=(8,7), dpi=100)
+    pareto_ax = pareto_fig.add_subplot(111)
+    # stockPareto, bestPick = yd.getOptionsData(0.9, 10000)
+    stockPareto = pd.read_pickle('stockParetaData0425.pk1')
+    stockPareto.plot(kind='scatter',x='POP',y='Potential Gain Multiple Contracts', legend = 'Stock Name', ax = pareto_ax)
+    pareto_ax.set_title('Pareto Curve for Best Options (Puts)')
+    pareto_ax.set_xlabel('Probability of Profit (%)')
+    pareto_ax.set_ylabel('Premium Collected')
     
     
+    # put pareto curve axes into tkinter GUI
+    canvas = FigureCanvasTkAgg(pareto_fig, master=root)
+    canvas.get_tk_widget().pack(side=tkinter.RIGHT, anchor=tkinter.NE)#, fill=tkinter.Y)#, expand=1)
+
+
+# canvas.create_oval(10, 10, 80, 80, outline="#f11",fill="#1f1", width=2)
+
+
+
+
+
+
+
+
+# # plot data to respective axes objects
+# # pareto_ax.plot(t, s)
+
+
+# ##
+
+
+
+
+
+# # adds in toolbar for paretoCurve plot
+# toolbar = NavigationToolbar2Tk(canvas, root)
+# toolbar.update()
+# canvas._tkcanvas.pack(side=tkinter.LEFT)#, fill=tkinter.BOTH, expand=1)
+
+
+def callback():
+    print("click!")
+
+
+
+def refresh():
+    print("refresh")
+
+# b = tkinter.Button(master=root, text="Quit", command=callback)
+# b.pack(side=tkinter.LEFT, anchor=tkinter.SW)
+# refresh = tkinter.Button(master=root, text = "Refresh", command=refresh)
+# refresh.pack(side=tkinter.LEFT, anchor=tkinter.SW)
+
+
 
 
 
@@ -218,24 +228,31 @@ if __name__ == '__main__':
     # stored = store_data()
     # print(stored)
     
-    stockPareto = pd.read_pickle('stockParetaData0425.pk1')
-    root = plotOptions(stockPareto)
-    textFrame = plotOptions.startGUI(root)
-    
-    tkinter.Label(textFrame, text="Risk Level").grid(row=1, column=1)#side=tkinter.LEFT, anchor=tkinter.SW)
-    Risk = tkinter.Entry(textFrame)
-    Risk.grid(row=1, column=2)#side=tkinter.LEFT, anchor=tkinter.SW)
-    tkinter.Label(textFrame, text="Budget").grid(row=2, column=1)#side=tkinter.LEFT, anchor=tkinter.SW)
-    Budget = tkinter.Entry(textFrame)
-    Budget.grid(row=2, column=2)#side=tkinter.LEFT, anchor=tkinter.SW)
-    Risk_num = Risk.get()
-    Budget_num = Budget.get()
-    go = tkinter.Button(textFrame, text='Enter', command=plotOptions.store_data(Risk_num, Budget_num))
-    go.grid(row=3, column=1, columnspan=2)
-    # Risk, Budget = plotOptions.GetInputs(textFrame)
+    root = startGUI()
+    textFrame = startTextInput(root)
+    Risk, Budger = GetInputs(textFrame)
     
     
-
+    
+    
+    # stockPareto = pd.read_pickle('stockParetaData0425.pk1')
+    # root = plotOptions(stockPareto)
+    # textFrame = plotOptions.startGUI(root)
+    
+    # tkinter.Label(textFrame, text="Risk Level").grid(row=1, column=1)#side=tkinter.LEFT, anchor=tkinter.SW)
+    # Risk = tkinter.Entry(textFrame)
+    # Risk.grid(row=1, column=2)#side=tkinter.LEFT, anchor=tkinter.SW)
+    # tkinter.Label(textFrame, text="Budget").grid(row=2, column=1)#side=tkinter.LEFT, anchor=tkinter.SW)
+    # Budget = tkinter.Entry(textFrame)
+    # Budget.grid(row=2, column=2)#side=tkinter.LEFT, anchor=tkinter.SW)
+    # Risk_num = Risk.get()
+    # Budget_num = Budget.get()
+    # go = tkinter.Button(textFrame, text='Enter', command=plotOptions.store_data(Risk_num, Budget_num, textFrame))
+    # go.grid(row=3, column=1, columnspan=2)
+    # # Risk, Budget = plotOptions.GetInputs(textFrame)
+    
+    
+    
     #getOptionsData(personalRiskTolerance, budget, printOutput = False)
     # charty = getDetailedQuote('DOW', ax)
     tkinter.mainloop()
